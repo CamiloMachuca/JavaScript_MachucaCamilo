70ee402a6b594060d0fc97fd467969234661b8c7
camilomachuca <camilomachuca03@gmail.com>
2024-08-21 17:10:15 -0500

    Feat :sparkles: A new function has been added

--- Dia3/script.js
+++ Dia3/script.js
@@ -2,9 +2,6 @@ const numero=prompt("Ingrese el numero:")// Se guarda el numero ingresado por el
 var bucleprincipal=true
 while (bucleprincipal==true){// Se crea un bucle para sacar el superDigit
     if (numero>10){
-        
-    
-
         function convertiralista(numero){// función para comvertir el numero en una lista
             const digitos=[];
             const numerostring=numero.toString();
@@ -14,16 +11,11 @@ while (bucleprincipal==true){// Se crea un bucle para sacar el superDigit
             }
             return digitos
         }
-
-
         const digitos= convertiralista(numero)
-        console.log(digitos)
         var totalNumero=0
         for (let i in digitos){//función para sumar los digitos del numero ingresado por el usuario
             totalNumero +=digitos[i]
         }
-    
-
         function convertiralista2(numero2){// función para comvertir el numero en una lista
             const digitos2=[];
             const numerostring=numero2.toString();
@@ -34,18 +26,31 @@ while (bucleprincipal==true){// Se crea un bucle para sacar el superDigit
             return digitos2
         }
         var numero2=totalNumero
-        const digitos2=convertiralista2(totalNumero)
-        console.log (digitos2)
-
+        const digitos2=convertiralista2(numero2)
         var totalNumero2=0
         for (let i in digitos2){//función para sumar los digitos del numero ingresado por el usuario
             totalNumero2 +=digitos2[i]
         }
-        console.log("El super Digit del numero",numero, "es",totalNumero2)// Se imprime el superDigit
+        function convertiralista3(numero3){// función para comvertir el numero en una lista
+            const digitos3=[];
+            const numerostring=numero3.toString();
+            for (let i =0; i < numerostring.length; i++){
+                const digito=parseInt(numerostring[i]);
+                digitos3.push(digito);
+            }
+            return digitos3
+        }
+        var numero3=totalNumero2
+        const digitos3=convertiralista2(numero3)
+        var totalNumero3=0
+        for (let i in digitos3){//función para sumar los digitos del numero ingresado por el usuario
+            totalNumero3 +=digitos3[i]
+        }
+        console.log("El super Digit del numero",numero, "es",totalNumero3)// Se imprime el superDigit
         bucleprincipal=false
 
     }else if(numero<10){
-        console.log("El super digito es:",numero)// Se imprime el superDigit
+        console.log("El super digit es:",numero)// Se imprime el superDigit
         bucleprincipal=false
     }
 }
