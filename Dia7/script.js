493f0330af774679679611417d166e58d4a82f3f
camilomachuca <camilomachuca03@gmail.com>
2024-08-29 19:34:10 -0500

    Feat :sparkles: Creation of new functions

--- Dia7/script.js
+++ Dia7/script.js
@@ -1,16 +1,17 @@
-document.getElementById('button1').addEventListener('click', getDataFromApi);
-function getDataFromApi(){
-    fetch('')
-    .then(res=> res.json())
-    .then(data=>{
-        console.log(typeof(data));
-        console.log(data[0]['https://superheroapi.com/api.php/a17b7d1498b611a8a388687a1db1da5b/ 2'])
-        let output='';
-        data.forEach(function(user){
-            output+=`<li>${user.login}</li>`
-        });
-        document.getElementById('output').innerHTML=output;
-    })
-}
 
-var Idbusqueda= document.getElementById('IDbusqueda').value;
+document.getElementById('enviar').addEventListener('click',function(){
+    const id=document.getElementById('idbusqueda').value;
+    fetch(`https://superheroapi.com/api.php/a17b7d1498b611a8a388687a1db1da5b/${id}`)
+    .then(Response => Response.json())
+    .then(Data => {
+
+        document.getElementById('datoss').innerHTML=JSON.stringify(Data,null,2);
+
+})
+
+
+})
+
+
+
+
