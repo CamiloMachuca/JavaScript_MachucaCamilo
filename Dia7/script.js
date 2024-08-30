91a37a1ed3514c0f028013af6fbc1c97073951c6
Camilomachuca <Camilomachuca03@gmail.com>
2024-08-30 10:21:04 -0500

    Feat :sparkles: Creation of new funtions

--- Dia7/script.js
+++ Dia7/script.js
@@ -22,38 +22,38 @@ document.getElementById('enviar').addEventListener('click',function(){
                     <th scope="col">${Data.id}</th>
                     <td scope="row">${Data.name}</td>
                     <td scope="col">
-                    <p>intelligence ${Data.powerstats.intelligence}</p>
-                    <p>strength ${Data.powerstats.strength}</p>
-                    <p>speed ${Data.powerstats.speed}</p>
-                    <p>durability ${Data.powerstats.durability}</p>
-                    <p>power ${Data.powerstats.power}</p>
-                    <p>combat ${Data.powerstats.combat}</p>
+                    <p><strong>intelligence</strong> ${Data.powerstats.intelligence}</p>
+                    <p><strong>strength</strong> ${Data.powerstats.strength}</p>
+                    <p><strong>speed </strong>${Data.powerstats.speed}</p>
+                    <p><strong>durability </strong>${Data.powerstats.durability}</p>
+                    <p><strong>power </strong>${Data.powerstats.power}</p>
+                    <p><strong>combat </strong>${Data.powerstats.combat}</p>
                     </td>
                     <td scope="col">
-                    <p>${Data.biography["full-name"]}</p>
-                    <p>${Data.biography["alter-egos"]}</p>
-                    <p>${Data.biography["aliase"]}</p>
-                    <p>${Data.biography["place-of-birth"]}</p>
-                    <p>${Data.biography["first-appearance"]}</p>
-                    <p>${Data.biography["publisher"]}</p>
-                    <p>${Data.biography["alignment"]}</p>
+                    <p><strong>Full-name:</strong> ${Data.biography["full-name"]}</p>
+                    <p><strong>Alter-egos:</strong> ${Data.biography["alter-egos"]}</p>
+                    <p><strong>Aliase</strong>${Data.biography["aliase"]}</p>
+                    <p><strong>place-of-birth:</strong> ${Data.biography["place-of-birth"]}</p>
+                    <p><strong>first-appearance:</strong> ${Data.biography["first-appearance"]}</p>
+                    <p><strong>publisher: </strong>${Data.biography["publisher"]}</p>
+                    <p><strong>alignment:</strong> ${Data.biography["alignment"]}</p>
                     </td>
                     
                     <td scope="col">
-                    <p>${Data.appearance["gender"]}</p>
-                    <p>${Data.appearance["race"]}</p>
-                    <p>${Data.appearance["height"]}</p>
-                    <p>${Data.appearance["weight"]}</p>
-                    <p>${Data.appearance["eye-color"]}</p>
-                    <p>${Data.appearance["hair-color"]}</p>
+                    <p><strong>gender:</strong>${Data.appearance["gender"]}</p>
+                    <p><strong>race:</strong> ${Data.appearance["race"]}</p>
+                    <p><strong>height:</strong>${Data.appearance["height"]}</p>
+                    <p><strong>weight:</strong>${Data.appearance["weight"]}</p>
+                    <p><strong>eye-color:</strong>${Data.appearance["eye-color"]}</p>
+                    <p><strong>hair-color:</strong>${Data.appearance["hair-color"]}</p>
                     </td>
                     <td scope="col">
-                    <p>${Data.work["occupation"]}</p>
-                    <p>${Data.work["base"]}<p>
+                    <p><strong>occupation:</strong> ${Data.work["occupation"]}</p>
+                    <p><strong>base: </strong>${Data.work["base"]}<p>
                     </td>
                     <td scope="col">
-                    <p>${Data.connections["group-affiliation"]}</p>
-                    <p>${Data.connections["relatives"]}</p>
+                    <p><strong>group-affiliation:</strong>${Data.connections["group-affiliation"]}</p>
+                    <p><strong>relatives:</strong>${Data.connections["relatives"]}</p>
                     </td>
                     <td scope="col">
                     <img src="${Data.image.url}" alt="">
