71bd003fc52b6257bea57a8d2328568bbb7a1191
camilomachuca <camilomachuca03@gmail.com>
2024-08-31 20:25:59 -0500

    Feat :sparkles: Creation of sub tables

--- Dia8/script.js
+++ Dia8/script.js
@@ -7,13 +7,17 @@ document.getElementById('enviar').addEventListener('click', function () {
     .then(Data => {
         var uurl= Data.homeworld
         var urrllm= Data.films
+        var urlEspecie=Data.species
+        var urlVehicles=Data.vehicles
+        var urlStarships=Data.starships
         
         fetch(uurl)
         .then(Response => Response.json())
         .then(Dataaa => {
             
             let datosOrdenados = `
-        <table class="table">
+            
+        <table id="TablaPrincipal" class="table ">
 
             <tr class="table-dark">
             <th scope="col">Nombre:</th>
@@ -54,7 +58,7 @@ document.getElementById('enviar').addEventListener('click', function () {
             <tr class="table-dark">
             <td scope:"row"><strong>homeworld:</strong>
             <td scope="col">
-            <table class="table" id="tablados">
+            <table class="table borderedondo" id="tablados">
             <td scope="col">
             <p>name: ${Dataaa.name}</p>
             <p>rotation_period: ${Dataaa.rotation_period}</p>
@@ -76,24 +80,37 @@ document.getElementById('enviar').addEventListener('click', function () {
             <tr class="table-dark">
             <td scope:"row"><strong>films:</strong>
             <td scope="col">
-            <table class="table" id="tablados">
-            <td scope="col">
             <div id="filmsContenedor"></div>
             </td>
-            </table>
             </td>
             </tr>
+
+            <tr class="table-dark">
+            <td scope:"row"><strong>species:</strong>
+            <td scope="col">
+            <div id="speciesContenedor"></div>
+            </td>
+            </td>
+            </tr>
+
+            <tr class="table-dark">
+            <td scope:"row"><strong>vehicles:</strong>
+            <td scope="col">
+            <div id="contenedorVehicles"></div>
+            </td>
+            </td>
+            </tr>
+
+            <tr class="table-dark">
+            <td scope:"row"><strong>starships:</strong>
+            <td scope="col">
+            <div id="contenedorStarships"></div>
+            </td>
+            </td>
+            </tr>
+           
             
             
-            <tr class="table-dark">
-            <th scope:"row">species:</th>
-            </tr>
-            <tr class="table-dark">
-            <th scope:"row">vehicles:</th>
-            </tr>
-            <tr class="table-dark">
-            <th scope:"row">starships:</th>
-            </tr>
             <tr class="table-dark">
             <th scope:"row">Created:</th>
             <td scope="col">${Data.created}</td>
@@ -103,7 +120,7 @@ document.getElementById('enviar').addEventListener('click', function () {
             <td scope="col">${Data.edited}</td>
             </tr>
             <tr class="table-dark">
-            <th scope:"row">Edited:</th>
+            <th scope:"row">Url:</th>
             <td scope="col">${Data.url}</td>
             </tr>
         
@@ -117,7 +134,8 @@ document.getElementById('enviar').addEventListener('click', function () {
         .then(Response=> Response.json())
         .then(Dataaa2=>{
             let nuevosDatos =`
-            <tr class="table">
+            
+            <table class="table" id="tablados2">
             <td scope="col">
             <p>Name: ${Dataaa2.title}</p>
             <p>Episode_id: ${Dataaa2.episode_id}</p>
@@ -129,13 +147,104 @@ document.getElementById('enviar').addEventListener('click', function () {
             <p>edited: ${Dataaa2.edited}</p>
             <p>url: ${Dataaa2.url}</p>
             </td>
-            </tr>
-            
+            </table>
+
             `
             document.getElementById('filmsContenedor').innerHTML+=nuevosDatos;
 
         })
         });
+        fetch(urlEspecie)
+        .then(Response=> Response.json())
+        .then(Dataa3=>{
+            let datosSpecies=`
+            
+            <table class="table" id="tablados">
+            <td scope="col">
+            <p>name: ${Dataa3.name}</p>
+            <p>Classification: ${Dataa3.classification}</p>
+            <p>Designation: ${Dataa3.designation}</p>
+            <p>Average height: ${Dataa3.average_height}</p>
+            <p>Skin colors: ${Dataa3.skin_colors}</p>
+            <p>Hair colors: ${Dataa3.hair_colors}</p>
+            <p>Eye colors: ${Dataa3.eye_colors}</p>
+            <p>Average lifespan: ${Dataa3.average_lifespan}</p>
+            <p>Language: ${Dataa3.language}</p>
+            <p>created: ${Dataa3.created}</p>
+            <p>Edited: ${Dataa3.edited}</p>
+            <p>url: ${Dataa3.url}</p>
+            </td>
+            </table>
+            `
+            document.getElementById('speciesContenedor').innerHTML+=datosSpecies;
+
+        })
+        urlVehicles.forEach(Vehicles => {
+            fetch(Vehicles)
+            .then(Response=> Response.json())
+            .then(Dataa4=>{
+                let DatosVehicles =`
+                
+                <table class="table" id="tablados2">
+                <td scope="col">
+                <p>Name: ${Dataa4.name}</p>
+                <p>Model: ${Dataa4.model}</p>
+                <p>Manufacturer: ${Dataa4.manufacturer}</p>
+                <p>Cost in credits: ${Dataa4.cost_in_credits}</p>
+                <p>Length: ${Dataa4.length}</p>
+                <p>Max atmosphering speed: ${Dataa4.max_atmosphering_speed}</p>
+                <p>crew: ${Dataa4.crew}</p>
+                <p>Passengers: ${Dataa4.passengers}</p>
+                <p>Cargo capacity: ${Dataa4.cargo_capacity}</p>
+                <p>Consumables: ${Dataa4.consumables}</p>
+                <p>Vehicle class: ${Dataa4.vehicle_class}</p>
+                <p>Created: ${Dataa4.created}</p>
+                <p>Edited: ${Dataa4.edited}</p>
+                <p>Url: ${Dataa4.url}</p>
+
+                </td>
+                </table>
+    
+                `
+                document.getElementById('contenedorVehicles').innerHTML+=DatosVehicles;
+    
+            })
+            });
+            urlStarships.forEach(Starships => {
+                fetch(Starships)
+                .then(Response=> Response.json())
+                .then(Dataa5=>{
+                    let DatosStarships =`
+                    
+                    <table class="table" id="tablados2">
+                    <td scope="col">
+                    <p>Name: ${Dataa5.name}</p>
+                    <p>Model: ${Dataa5.model}</p>
+                    <p>Manufacturer: ${Dataa5.manufacturer}</p>
+                    <p>Cost in credits: ${Dataa5.cost_in_credits}</p>
+                    <p>Length: ${Dataa5.length}</p>
+                    <p>Max atmosphering speed: ${Dataa5.max_atmosphering_speed}</p>
+                    <p>crew: ${Dataa5.crew}</p>
+                    <p>Passengers: ${Dataa5.passengers}</p>
+                    <p>Cargo capacity: ${Dataa5.cargo_capacity}</p>
+                    <p>Consumables: ${Dataa5.consumables}</p>
+                    <p>Hyperdrive rating: ${Dataa5.hyperdrive_rating}</p>
+                    <p>MGLT: ${Dataa5.MGLT}</p>
+                    <p>Starship class: ${Dataa5.starship_class}</p>
+                    <p>Created: ${Dataa5.created}</p>
+                    <p>Edited: ${Dataa5.edited}</p>
+                    <p>Url: ${Dataa5.url}</p>
+
+    
+                    </td>
+                    </table>
+        
+                    `
+                    document.getElementById('contenedorStarships').innerHTML+=DatosStarships;
+        
+                })
+                });
+        
         
     })
 })
